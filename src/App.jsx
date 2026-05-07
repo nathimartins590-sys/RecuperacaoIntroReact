f8cf644e1e6f28f9b1cfffbe310d4c5bb649740b
Nathalia <nathimartins590@gmail.com>
2026-05-07 19:32:47 -0300

    mudança

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import Header from "./components/Header";
 import Home from './pages/Home';
-import ScriptAtividade1 from './pages/Atividade1';
-import Atividade1 from './pages/Atividade1';
-import Atividade2 from './Pages/Atividade2';
-import Atividade3 from './Pages/Atividadae3';
 
 function App() {
     return (
